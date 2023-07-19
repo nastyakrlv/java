af1d5ffe66f9e49d3fd6033a50c0ab371eced5cc
akim-berezhnoy <berezhnoy.akim@gmail.com>
2023-07-20 03:10:50 +0400

    reviewed

--- solutions/scan/Scan.java
+++ solutions/scan/Scan.java
@@ -10,13 +10,13 @@ import java.util.regex.Pattern;
 public class Scan {
     private final BufferedReader reader;
     private char[] buffer;
-    private int flag;
+    private int flag; // unused variable
     private int usefulBufferLength;
     private int bufferIndex;
     private boolean endOfFile;
-    private final int  size;
+    private final int  size; // 1) OPT+CMD+L 2) defaultSize
 
-//    private final Pattern INT_PATTERN;
+    private final Pattern INT_PATTERN;
 
     public Scan(InputStream inputStream) { // Хорошо
         reader = new BufferedReader(new InputStreamReader(inputStream));
@@ -24,14 +24,14 @@ public class Scan {
         buffer = new char[size];
         usefulBufferLength = bufferIndex = flag = 0;
         endOfFile = false;
-//        INT_PATTERN = Pattern.compile("^((-?[1-9]\\d{0,9})|0)$");//TODO: АКИМ проверить что быстрее isValidInt или try catch
+        INT_PATTERN = Pattern.compile("^((-?[1-9]\\d{0,9})|0)$");//TODO: АКИМ проверить что быстрее isValidInt или try catch
     }
 
     public Scan(String inputStream) { // Хорошо
         this(new ByteArrayInputStream(inputStream.getBytes()));
     }
 
-    public boolean hasNextLine() throws IOException {
+    public boolean hasNextLine() throws IOException { // Хорошо
         return hasNextChar() && !endOfFile;
     }
 
@@ -61,15 +61,8 @@ public class Scan {
             }
             char symbol = nextChar();
             if (Character.isWhitespace(symbol) || endOfFile) {
-                if (!lineBuffer.isEmpty()) {
-//                    hasNumber = isValidInt(lineBuffer.toString()); //TODO: АКИМ проверить что быстрее isValidInt или try catch
-//                    break;
-                    try {
-                        Integer.parseInt(lineBuffer.toString());
-                        hasNumber = true;
-                    } catch (NumberFormatException e) {
-                        hasNumber = false;
-                    }
+                if (!lineBuffer.isEmpty()) { // Можно упростить, избежать ифа и бреаков используя continue и return.
+                    hasNumber = isValidInt(lineBuffer.toString());
                     break;
                 } else if (endOfFile && lineBuffer.isEmpty()) {
                     break;
@@ -82,28 +75,27 @@ public class Scan {
         return hasNumber;
     }
 
-
     public int nextInt() throws IOException {
         StringBuilder lineBuffer = new StringBuilder();
-        while (hasNextChar()) { // TODO: for loop *пробовала уже, но стало хуже - сначала проверяем потом считываем
+        while (hasNextChar()) { // Похожая логика выше. Упростить.
             char symbol = nextChar();
             if (Character.isWhitespace(symbol) ) {
-                if (!lineBuffer.isEmpty()) { // TODO: !empty() *
+                if (!lineBuffer.isEmpty()) {
                     break;
                 }
             } else {
                 lineBuffer.append(symbol);
             }
         }
-        try { // good//TODO: АКИМ проверить что быстрее isValidInt или try catch
-            refillBuffer();
+        refillBuffer();
+        if (isValidInt(lineBuffer.toString())) {
             return Integer.parseInt(lineBuffer.toString());
-        } catch (NumberFormatException e) {
-            throw new InputMismatchException(e.getLocalizedMessage());
+        } else {
+            throw new InputMismatchException("дописать");
         }
     }
 
-    private char nextChar() {
+    private char nextChar() { // Хорошо
         return buffer[bufferIndex++];
     }
 
@@ -114,34 +106,35 @@ public class Scan {
             }
             refillBuffer();
         }
-        return bufferIndex < usefulBufferLength;
+        return bufferIndex < usefulBufferLength; // Судя по бренчам метода refillBuffer исход данного сравнения однозначен
     }
 
-    private void refillBuffer() throws IOException {
-        boolean ok = false;
+    private void refillBuffer() throws IOException { // Этот метод можно значительно упростить, и не только там, где я расставил комментарии
+        boolean ok = false; // Эта переменная не нужна, читать комменты ниже
         int usefulCharacters = usefulBufferLength - bufferIndex;
         if (usefulCharacters > 0) {
-            if (usefulCharacters > size) {
-                ok = true;
-                char[] temp = new char[usefulCharacters];
-                System.arraycopy(buffer, bufferIndex, temp, 0, usefulCharacters);
-                buffer = temp;
-                usefulBufferLength = usefulCharacters;
-                bufferIndex = 0;
-            } else if (usefulBufferLength > size)  {
+            if (usefulCharacters > size) { // Этот бранч никогда не выполнится
+                assert(false);
+//                ok = true;
+//                char[] temp = new char[usefulCharacters];
+//                System.arraycopy(buffer, bufferIndex, temp, 0, usefulCharacters);
+//                buffer = temp;
+//                usefulBufferLength = usefulCharacters;
+//                bufferIndex = 0;
+            } else if (usefulBufferLength > size)  { // В чем семантический смысл не делать это всегда?
                 char[] temp = new char[size];
                 System.arraycopy(buffer, bufferIndex, temp, 0, usefulCharacters);
                 buffer = temp;
                 usefulBufferLength = usefulCharacters;
                 bufferIndex = 0;
             }
-        } else {
+        } else { // Эту ветку можно заифать в начале и смерджить с условием ниже
             ok = true;
             buffer = new char[size];
             usefulBufferLength = 0;
             bufferIndex = 0;
         }
-        if (ok) {
+        if (ok) { // Наверх
             int count = reader.read(buffer, usefulBufferLength, buffer.length - usefulBufferLength);
             if (count != -1) {
                 usefulBufferLength += count;
@@ -156,8 +149,9 @@ public class Scan {
     private void increaseBuffer() throws IOException {
         if (bufferIndex >= buffer.length) {
                 buffer = Arrays.copyOf(buffer, buffer.length * 2);
-            }
-        int count = reader.read(buffer, bufferIndex, buffer.length - bufferIndex);
+            } // OPT+CMD+L
+        // COPY-PASTE 16 lines above
+        int count = reader.read(buffer, bufferIndex, buffer.length - bufferIndex); // Почему не defaultSize? Мы же договорились считывать блоками по defaultSize?
         if (count != -1) {
             usefulBufferLength += count;
         } else {
@@ -165,67 +159,16 @@ public class Scan {
         }
     }
 
-    public void close() throws IOException { reader.close(); }
+    public void close() throws IOException { reader.close(); } // Тогда бы уж реализовала Auto-Closable
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//    // TODO: обработай ошибку захода за конец потока (если тебя вызовут с flag == -1)
-//    protected char nextChar() throws IOException {
-//        // TODO: весь поток всегда хранится в памяти, переделать
-//        /*
-//           Увеличивай буффер только в методах вида "hasNext*",
-//           в противном случае используй дефолтный размер.
-//           P.S.: ужимай буфер, если тот стал большим и ты добежала его до конца.
-//
-//           Подсказка: сделай 2 метода
-//
-//           1) метод, который проверяет, есть ли впереди последовательность символов, подходящих под
-//           заданный тобой критерий, при этом не двигая никак buffer_iterator, не забывая символы сзади,
-//           но, при надобности, бесконтрольно расширяя буффер, кладя вперёд всё новые и новые блоки символов.
-//
-//           2) метод, который отдаёт тебе следующий символ, забывая предыдущие и при переходе за край буффера
-//           не просто читает символы из потока кладя их наверх и заполняя себя полнсотью как <метод 1 name>,
-//           а перечитывает символы в себя же, поверх старых. При этом, при заходе за край useful символов, пусть он
-//           перед заполнением проверяет, не стал ли он слишком большим после вызовов <метод 1 name> и сжмается,
-//           если увидит это.
-//         */
-//        if (bufferIndex >= usefulBufferLength) {
-//            if (bufferIndex >= buffer.length) {
-//                buffer = Arrays.copyOf(buffer, buffer.length * 2);
-//            }
-//            flag = reader.read(buffer, bufferIndex, buffer.length - bufferIndex);
-//            usefulBufferLength += flag;
-//            if (flag == -1) {
-//                reader.close();
-//                usefulBufferLength++;
-//            }
-//        }
-//        return buffer[this.bufferIndex++];
-//    }
-
-//    private boolean isValidInt(String pat) {//TODO: АКИМ проверить что быстрее isValidInt или try catch
-//        long num;
-//        if (INT_PATTERN.matcher(pat).matches()) {
-//            num =  Long.parseLong(pat);
-//            return (Integer.MAX_VALUE >= num) && (Integer.MIN_VALUE <= num);
-//        }
-//        return false;
-//    }
+    private boolean isValidInt(String pat) {
+        long num;
+        if (INT_PATTERN.matcher(pat).matches()) {
+            num =  Long.parseLong(pat);
+            return (Integer.MAX_VALUE >= num) && (Integer.MIN_VALUE <= num);
+        }
+        return false;
+    }
 
 }
 
